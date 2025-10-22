685efc4790d99cc5ba2189aa1e9a61527877dc42
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-10-22 20:25:09 +0300

    init: adding initial files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+    devServer: {
+    static: "./dist",           // or wherever your HTML lives
+    open: true,                 // auto-open browser
+  },
+  
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
+    ],
+};
